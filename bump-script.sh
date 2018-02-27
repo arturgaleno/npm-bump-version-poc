bc19c6ec980617813c0c4cb10417be780d95ed50
Artur Galeno <arturgalenom@gmail.com>
2018-02-27 00:58:31 -0300

    adding bump script

---
+++ bump-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://www.github.com/arturgaleno/npm-bump-version-poc
+cd npm-bump-version-poc
+npm version minor
+CURRENT_VERSION=$(npm run version --silent)
+git commit -m "updating version"
+git push origin master
+git checkout fix
+git merge master
+git push origin fix
+git tag $CURRENT_VERSION
+git push origin $CURRENT_VERSION
+docker build . -t myimage:$CURRENT_VERSION
